386e6ceae58dd16f5a8f90a3bbe58b63e895c136
Siva Balusu <Sbalusu1882@conestogac.on.ca>
2020-09-11 22:27:33 -0400

    Internal working JSX,using of React.careateElement

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to React</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Welcome to React</h1>
+  //   </div>
+  // );
+  //Internal working of above code
+  return React.createElement("div",{className:"App"},React.createElement("h1",null,"Welcome to React"));   
 }
 
 export default App;
