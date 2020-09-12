c64f0633b456b5c7ca655a99c1fe540185e2025b
Siva Balusu <Sbalusu1882@conestogac.on.ca>
2020-09-12 10:57:59 -0400

    using state to pass values in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
+class App extends Component {
+  state={
+    persons: [
+      {name:'siva',age:24},
+      {name:'raju',age:22},
+      {name:'salman',age:20}
+    ],
+    otherstate:'otherValue'
+  };
+ render() {
+   return (
     <div className="App">
       <h1>Welcome to React</h1>
-      <Person name="siva" age="24"/>
-      <Person name="raju" age="22">How are you doing</Person>
-      <Person name="salman" age="19"/>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>How are you doing</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
   );
   //Internal working of above code
   //return React.createElement("div",{className:"App"},React.createElement("h1",null,"Welcome to React"));   
+}
 }
 
 export default App;
