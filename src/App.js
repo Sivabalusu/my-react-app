ef2b122788f27e7b7c23718f76f74c2ae8d1d3d7
Siva Balusu <Sbalusu1882@conestogac.on.ca>
2020-09-11 22:52:27 -0400

    Added person component and rendered it in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Welcome to React</h1>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <Person/>
+    </div>
+  );
   //Internal working of above code
-  return React.createElement("div",{className:"App"},React.createElement("h1",null,"Welcome to React"));   
+  //return React.createElement("div",{className:"App"},React.createElement("h1",null,"Welcome to React"));   
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person=()=>{
+    return(
+        <div>
+            <p>Hello, this is Siva</p>
+            <p>My age is 24</p>
+        </div>
+        );
+}
+export default person;
